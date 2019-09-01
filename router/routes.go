1c6bd6229c061f6313922863921d5039394df2a8
benitogf <benitogf@outlook.com>
2019-09-01 14:26:00 +0800

    audit map

--- router/routes.go
+++ router/routes.go
@@ -1,7 +1,6 @@
 package router
 
 import (
-	"github.com/gorilla/mux"
 	"net/http"
 	"github.com/benitogf/katamari"
 )
@@ -9,16 +8,14 @@ import (
 // Routes definition
 func Routes(server *katamari.Server) {
 	// Filters
-	katamari.OpenFilter(server, "boxes/*")
-	katamari.OpenFilter(server, "things/*/*/*") // thing/boxid/userid/id
-	katamari.OpenFilter(server, "mails/*")
-	katamari.OpenFilter(server, "posts/*")
+	server.OpenFilter("boxes/*")
+	server.OpenFilter("things/*/*/*") // thing/boxid/userid/id
+	server.OpenFilter("mails/*")
+	server.OpenFilter("posts/*")
 	server.ReadFilter("blog", blogFilter)
-	katamari.OpenFilter(server, "stocks/*/*")
-	katamari.OpenFilter(server, "market/*")
+	server.OpenFilter("stocks/*/*")
+	server.OpenFilter("market/*")
 
-	// Server - Routes
-	server.Router = mux.NewRouter()
 	server.Router.HandleFunc("/blog", func(w http.ResponseWriter, r *http.Request) {
 		blogStream(server, w, r)
 	}).Methods("GET")
